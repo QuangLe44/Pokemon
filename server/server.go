6b92514b71bc9f4b088a3f095aa6655f8fa19b85
ITITIU20035 <81848609+ITITIU20035@users.noreply.github.com>
2024-06-12 00:37:54 +0700

    Add pokemon struct for pokedex

--- server/server.go
+++ server/server.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"math/rand"
 	"net"
 	"os"
 	"os/signal"
@@ -30,9 +28,7 @@ type player struct {
 }
 
 type pokemon struct {
-	id    int
-	level int
-	ev    float64
+	Name string `json:"name"`
 }
 
 func newServer(address string, size int, pokedex []*pokemon) (*server, error) {
@@ -81,63 +77,13 @@ func (s *server) handleConnections() {
 }
 func (s *server) handleConnection(conn net.Conn) {
 	defer conn.Close()
-	playerID := fmt.Sprintf("player-%d", len(s.players))
-	player := &player{
-		id:       playerID,
-		x:        rand.Intn(len(s.pokeworld)),
-		y:        rand.Intn(len(s.pokeworld)),
-		pokemons: make([]*pokemon, 0, 200),
-	}
-	s.players[playerID] = player
-	fmt.Fprintf(conn, "Welcome to Pokeworld, %s! You are at (%d, %d).\n", playerID, player.x, player.y)
-
-	go func() {
-		for {
-			select {
-			case <-s.shutdown:
-				return
-			default:
-				if player.autoMode {
-					player.move(s)
-				}
-				time.Sleep(time.Second)
-			}
-		}
-	}()
-
-	scanner := bufio.NewScanner(conn)
-	for scanner.Scan() {
-		cmd := scanner.Text()
-		switch cmd {
-		case "up":
-			player.moveUp(s)
-			fmt.Fprintf(conn, "You moved up! You are at (%d, %d).\n", player.x, player.y)
-		case "down":
-			player.moveDown(s)
-			fmt.Fprintf(conn, "You moved down! You are at (%d, %d).\n", player.x, player.y)
-		case "left":
-			player.moveLeft(s)
-			fmt.Fprintf(conn, "You moved left! You are at (%d, %d).\n", player.x, player.y)
-		case "right":
-			player.moveRight(s)
-			fmt.Fprintf(conn, "You moved right! You are at (%d, %d).\n", player.x, player.y)
-		case "auto":
-			player.autoMode = true
-			player.autoTimer = time.AfterFunc(120*time.Second, func() {
-				player.autoMode = false
-			})
-			fmt.Fprint(conn, "Auto mode enabled.\n")
-		default:
-			fmt.Fprint(conn, "Invalid command.\n")
-		}
-	}
+	go startPokeworld(s, conn)
 }
 
 func (s *server) Start() {
 	s.wg.Add(2)
 	go s.acceptConnections()
 	go s.handleConnections()
-	go s.spawnPokemon()
 }
 func (s *server) Stop() {
 	close(s.shutdown)
@@ -157,75 +103,20 @@ func (s *server) Stop() {
 	}
 }
 
-func (s *server) spawnPokemon() {
-	for {
-		select {
-		case <-s.shutdown:
-			return
-		default:
-			for i := 0; i < 50; i++ {
-				pokemon := &pokemon{
-					id:    rand.Intn(len(s.pokedex)),
-					level: rand.Intn(100) + 1,
-					ev:    0.5 + rand.Float64(),
-				}
-				x, y := rand.Intn(len(s.pokeworld)), rand.Intn(len(s.pokeworld))
-				s.pokeworld[x][y] = pokemon
-				fmt.Println("Spawned pokemon", pokemon.id, "at (", x, ", ", y, ")")
-			}
-			time.Sleep(time.Minute)
-		}
-	}
-}
-
-func (p *player) move(s *server) {
-	dx, dy := rand.Intn(3)-1, rand.Intn(3)-1
-	p.x += dx
-	p.y += dy
-	if p.x < 0 {
-		p.x = 0
-	} else if p.x >= len(s.pokeworld) {
-		p.x = len(s.pokeworld) - 1
-	}
-	if p.y < 0 {
-		p.y = 0
-	} else if p.y >= len(s.pokeworld) {
-		p.y = len(s.pokeworld) - 1
-	}
-}
-
-func (p *player) moveUp(s *server) {
-	p.y--
-	if p.y < 0 {
-		p.y = 0
-	}
-}
-
-func (p *player) moveDown(s *server) {
-	p.y++
-	if p.y >= len(s.pokeworld) {
-		p.y = len(s.pokeworld) - 1
-	}
-}
-
-func (p *player) moveLeft(s *server) {
-	p.x--
-	if p.x < 0 {
-		p.x = 0
-	}
-}
-
-func (p *player) moveRight(s *server) {
-	p.x++
-	if p.x >= len(s.pokeworld) {
-		p.x = len(s.pokeworld) - 1
-	}
-}
-
 func main() {
-	pokedex := make([]*pokemon, 100)
-	for i := range pokedex {
-		pokedex[i] = &pokemon{id: i}
+	pokemons, err := loadPokemonData("../data/baseInfo.json")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	pokedex := make([]*pokemon, len(pokemons))
+	for i, p := range pokemons {
+		pokedex[i] = &pokemon{Name: p.Name}
+	}
+
+	fmt.Println("Pokédex:")
+	for _, p := range pokedex {
+		fmt.Println(p.Name)
 	}
 	s, err := newServer(":8080", 1000, pokedex)
 	if err != nil {
--- server/session.go
+++
@@ -1,9 +0,0 @@
-package main
-
-type GameSession struct {
-	sessionID int `json:"id"`
-}
-
-func createGameSession(server *Server) {
-	//Not Implemented..
-}
